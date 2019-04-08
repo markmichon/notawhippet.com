9d523149817c20b43434e42284c82fb298429a84
markmichon <hello@markmichon.com>
2019-04-08 17:30:53 -0400

    update config to remove starter code

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,15 +21,15 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
+    // `gatsby-transformer-sharp`,
+    // `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
     // {
     //   resolve: `gatsby-plugin-manifest`,
