b34205e65fb7c9f1927e049eb983ba2977765bc1
Mark Michon <hello@markmichon.com>
2019-06-11 09:58:04 -0400

    Add image type to resolve safari issue. (#6)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,22 @@
-const proxy = require('http-proxy-middleware')
+const proxy = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
-    title: 'Not a Whippet',
-    description:"",
-    author: "@markmichon"
+    title: "Not a Whippet",
+    description: "",
+    author: "@markmichon",
   },
 
   developMiddleware: app => {
     app.use(
-      '/.netlify/functions',
+      "/.netlify/functions",
       proxy({
-        target: 'http://localhost:9000',
+        target: "http://localhost:9000",
         pathRewrite: {
-          '/.netlify/functions/': '',
-        }
+          "/.netlify/functions/": "",
+        },
       })
     )
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    
-  ]
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-styled-components"],
 }
